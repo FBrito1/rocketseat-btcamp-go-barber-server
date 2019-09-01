f0d53b01ac6b6c1dd57bcb6bf354960407e95f96
Fbrito1 <luan.tecno@gmail.com>
2019-09-01 20:20:42 -0300

    add NotificationController | mongo configuration | Notification schema

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,38 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(req, res) {
+    const checkProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkProvider) {
+      return res.status(401).json({
+        error: 'Only providers can load notifications',
+      });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const notification = await Notification.findByIdAndUpdate(
+      id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();

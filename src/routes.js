89fb8dab49c8a8fe7c13aeb8ea6bda256023bc97
Fbrito1 <luan.tecno@gmail.com>
2019-09-01 18:47:53 -0300

    list provider appointments

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionsController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,6 +25,8 @@ routes.put('/users', UserController.update);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

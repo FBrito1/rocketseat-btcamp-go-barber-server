32e6ea137e8eac70cf28a65dd5af25882c130f8b
Fbrito1 <luan.tecno@gmail.com>
2019-09-01 21:14:11 -0300

    add cancellation appointment mail feature

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '38fe8bc232e6b1',
+    pass: '653dc45cfc717b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
